1dc2954754d0263016b9e249d009bb313bfcd459
shlyapp <shlyapp.exe@gmail.com>
2021-09-07 16:16:17 +0400

    add answer

--- kpolyakov/Task 2/65.py
+++ kpolyakov/Task 2/65.py
@@ -5,3 +5,5 @@ for x in range(2):
         for z in range(2):
             if ((not x) and y and z) or ((not x) and y and (not z)) or ((not x) and (not y) and (not z)):
                 print(y, x, z)
+
+# yxz
