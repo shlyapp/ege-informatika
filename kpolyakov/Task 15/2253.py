14e69d016f259e1768b2b6978e7cf8df5e415789
shlyapp <shlyapp.exe@gmail.com>
2021-09-08 09:59:22 +0400

    2253 test

---
+++ kpolyakov/Task 15/2253.py
@@ -0,0 +1,10 @@
+k = 100000
+
+for A in range(1, k):
+    z = True
+    for x in range(1, k):
+        if ((x%A==0) <= ((x%A==0) <= ((x%34==0) and (x%51==0)))) == 0:
+            z = False
+            break
+    if (z):
+        print(A)
