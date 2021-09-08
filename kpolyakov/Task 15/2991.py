ddf44feefb72aa26e77caded4ef339b2a48a012a
shlyapp <shlyapp.exe@gmail.com>
2021-09-08 22:29:38 +0400

    2991 fix

--- kpolyakov/Task 15/2991.py
+++ kpolyakov/Task 15/2991.py
@@ -1,10 +1,11 @@
-for A in range(1, 1000):
+for A in range(1, 100000):
     flag = True
-    for x in range(1, 1000):
+    for x in range(1, 100000):
         if ( ( ( (x%A==0) and (x%45==0) ) <= (x%162==0) ) and (A > 200) ) == 0:
             flag = False
             break
     if (flag):
         print(A)
+        break
         
         
