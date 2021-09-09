15578af65bb92f7191e88c15cf35574b1d9342f0
shlyapp <shlyapp.exe@gmail.com>
2021-09-09 17:05:45 +0400

    11338 task

---
+++ inf-ege/Task 2/11338.py
@@ -0,0 +1,8 @@
+print("x y z w")
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if ( x and (not y) and ((not z) or w)):
+                    print(x, y, z, w)
