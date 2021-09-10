e3717e13e2f368b13d61b5b57b82f693cb409878
shlyapp <shlyapp.exe@gmail.com>
2021-09-10 13:03:10 +0400

    380 task

---
+++ kpolyakov/Task 15/380.py
@@ -0,0 +1,13 @@
+
+p = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+q = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+
+A = set(sorted(p + q))
+
+counter = 0
+
+for x in A:
+    if (x in p) and (x in q):
+        counter+=1
+
+print(counter)
