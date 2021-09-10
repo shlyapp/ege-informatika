a65f425ad447f238bd207614281ae57489e75c7c
shlyapp <shlyapp.exe@gmail.com>
2021-09-10 16:22:38 +0400

    386 task

---
+++ kpolyakov/Task 15/386.py
@@ -0,0 +1,12 @@
+P = [1, 3, 4, 9, 11, 13, 15, 17, 19, 21]
+Q = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+
+A = set(sorted(P+Q))
+
+counter = 0
+
+for x in A:
+    if ((x in P) and (x in Q)):
+        counter += 1
+
+print(counter)
