0afc6cea7f71a28da7a28a41e46cdab64a75a45b
shlyapp <shlyapp.exe@gmail.com>
2021-10-05 21:24:56 +0400

    update 4344 task

--- kpolyakov/Task 9/4344.py
+++ kpolyakov/Task 9/4344.py
@@ -1,16 +1,15 @@
 def is_trapecia(a):
 
-    if (a[0]+a[1] == a[2]+a[3]):
+    if ((a[0]+a[1]==180 and a[2]+a[3]==180)) and (a[0]!=a[2] and a[1]!=a[3]) and (a[0]!=a[1]!=a[2]!=a[3]):
+        print(*a)
         return True
     
-    
-
 file = open("kpolyakov/Task 9/9-123.csv")
 count = 0
 for s in file:
     a = list(map(int, s.split(';')))
-    if (sum(a)==360 and a[0]!=a[1]!=a[2]!=a[3] and is_trapecia(a)):
+    if (sum(a)==360 and is_trapecia(a)):
         count+=1
-        print(*a)
+        #print(*a)
 
 print(count)
