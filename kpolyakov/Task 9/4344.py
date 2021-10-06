b5d3db528a1dce75a661ac453cae71c338890cc0
shlyapp <shlyapp.exe@gmail.com>
2021-10-06 15:52:21 +0400

    4344 right answer

--- kpolyakov/Task 9/4344.py
+++ kpolyakov/Task 9/4344.py
@@ -1,30 +1,12 @@
 def is_trapecia(a):
-
-    #if (a[0]+a[1]==180 and a[2]+a[3]==180) or (a[0]+a[2]==180 and a[1]+a[3]==180):
-        #if (a[0]<=90 and a[3]<=90) and (a[1]>=90 and a[2]>=90):
-           #return True
-    #else:
-        #print(*a)
-        
-    #if ((a[0]+a[1]==180 and a[2]+a[3]==180) and (a[0]!=a[2] and a[1]!=a[3])) or ((a[0]+a[3]==180 and a[1]+a[2]==180) and (a[0]!=a[1] and a[2]!=a[3])):
-        #return True
-    #else:
-        #print(*a)
-
-    if (a[0]>=90 and a[1]<=90 and a[2]<=90 and a[3]>=90) or (a[0]<=90 and a[1]>=90 and a[2]>=90 and a[3]<=90):
-        if (a[0]==90 or a[1]==90 or a[2]==90 or a[3]==90):
-            print(*a)
-        return True
+       if(a[0]!=a[2]) and (a[0]+a[1] == a[2] + a[3] == 180 or a[0]+a[3] == a[1] + a[2] == 180):
+           return True
    
-
-
-    
 file = open("kpolyakov/Task 9/9-123.csv")
 count = 0
 for s in file:
     a = list(map(int, s.split(';')))
-    if (sum(a)==360 and is_trapecia(a) and a[0]!=a[1]!=a[2]!=a[3]):
+    if (sum(a)==360 and is_trapecia(a)):
         count+=1
-        #print(*a)
 
 print(count)
