241dd51348e9977ee5192a12c3fbd66b84a87d58
shlyapp <shlyapp.exe@gmail.com>
2021-10-10 15:45:27 +0400

    17 task

---
+++ Testers/210913/17.py
@@ -0,0 +1,13 @@
+file = open('Testers/210913/17.txt')
+data = []
+for elem in file:
+    data.append(elem[:-1])
+
+counter = 0
+maximum = 0
+for i in range(0, len(data)-1):
+    if (int(data[i])%3==0 or int(data[i+1])%3==0):
+        counter +=1
+        maximum = max(maximum, int(data[i])+int(data[i+1]))
+
+print(counter, maximum)
