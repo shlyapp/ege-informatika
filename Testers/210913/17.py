15c51e91ed71b4de9079f7c6784145ba109b9fa6
shlyapp <shlyapp.exe@gmail.com>
2021-10-10 22:53:55 +0400

    update 17

--- Testers/210913/17.py
+++ Testers/210913/17.py
@@ -4,10 +4,10 @@ for elem in file:
     data.append(elem[:-1])
 
 counter = 0
-maximum = 0
+maximum = []
 for i in range(0, len(data)-1):
     if (int(data[i])%3==0 or int(data[i+1])%3==0):
         counter +=1
-        maximum = max(maximum, int(data[i])+int(data[i+1]))
+        maximum.append(int(data[i])+int(data[i+1]))
 
-print(counter, maximum)
+print(counter, maximum[-1])
