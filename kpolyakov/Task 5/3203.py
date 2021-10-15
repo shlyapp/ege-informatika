039d151cbbd0458385e7eb37debf815557b9c473
shlyapp <shlyapp.exe@gmail.com>
2021-10-15 12:50:09 +0400

    3203 task

---
+++ kpolyakov/Task 5/3203.py
@@ -0,0 +1,10 @@
+def F(n):
+    num = bin(n)[2:]
+    num = num + num[-2]
+    num = num + num[1]
+    return int(num, 2)
+
+for i in range(11, 1000):
+    if F(i) > 100:
+        print(i)
+        break
