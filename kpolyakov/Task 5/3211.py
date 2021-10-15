0b1f4b871796ab81c9733e65227093eaf496dc73
shlyapp <shlyapp.exe@gmail.com>
2021-10-15 15:57:37 +0400

    3211 task

--- kpolyakov/Task 5/3211.py
+++ kpolyakov/Task 5/3211.py
@@ -1,8 +1,17 @@
-k=0
-for x in range(3,1000):
-    num=bin(x)[2:]
-    num=num+num[-2]+num[1]
-    z=int(num,2)
-    if 149<z<251:
-        k+=1
-print(k)
+def F(x):
+    num = bin(x)[2:]
+
+    for i in range(3):
+        if (num.count('0') == num.count('1')):
+            num += num[-1]
+        elif (num.count('0') > num.count('1')):
+            num += '1'
+        else:
+            num += '0'
+
+    z = int(num, 2)
+    return z
+
+for i in range(1, 80):
+    if (F(i)%2==0 and F(i)%4!=0):
+        print(i)
