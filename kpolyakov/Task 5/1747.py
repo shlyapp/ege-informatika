0df164318547c9b65cc0b033558731e8d138632f
shlyapp <shlyapp.exe@gmail.com>
2021-10-17 18:28:31 +0400

    1747 fix

--- kpolyakov/Task 5/1747.py
+++ kpolyakov/Task 5/1747.py
@@ -1,11 +1,17 @@
 def F(n):
     num = bin(n)[2:]
+    while len(num) < 8:
+        num = '0' + num
+    index_last = len(num) - num[::-1].find('1') - 1
     num2 = ''
-    for letter in num:
-        if (letter == '1'): num2 += '0'
-        else: num2 += '1'
+    for i in range(len(num)):
+        if (i < index_last):
+            if (num[i] == '0'): num2 += '1'
+            if (num[i] == '1'): num2 += '0'
+        else:
+            num2 += num[i]
     return int(num2, 2)
 
 for i in range(0, 256):
     if (F(i) == 98):
-        print(i)
+       print(i)
