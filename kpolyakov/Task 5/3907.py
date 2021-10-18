35631fa4109e093a239a9b9c6a39af090521f722
shlyapp <shlyapp.exe@gmail.com>
2021-10-18 23:47:29 +0400

    3907 tasj

---
+++ kpolyakov/Task 5/3907.py
@@ -0,0 +1,17 @@
+def F(x):
+    num = bin(x)[2:]
+
+    for i in range(3):
+        if (num.count('0') == num.count('1')):
+            num += num[-1]
+        elif (num.count('0') > num.count('1')):
+            num += '1'
+        else:
+            num += '0'
+
+    z = int(num, 2)
+    return z
+
+for i in range(1, 100):
+    if (F(i)%4!=0 and F(i)%2==0):
+        print(i)
