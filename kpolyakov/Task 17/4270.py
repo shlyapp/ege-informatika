489bb17ea1828edf97841a0ead21992122b6a9f5
shlyapp <shlyapp.exe@gmail.com>
2021-11-06 21:19:55 +0400

    4270 task

--- kpolyakov/Task 17/4270.py
+++ kpolyakov/Task 17/4270.py
@@ -4,14 +4,11 @@ for elem in file:
     data.append(int(elem[:-1]))
 
 counter = 0
-minimum = max(data)
+maximum = min(data)
 
 for i in range(len(data) - 1):
-    if (str(data[i])[-1] == '6' and data[i] % 3 == 0) or (str(data[i + 1])[-1] == '6' and data[i+1] % 3 == 0):
+    if (data[i] % 9 == 0 and data[i+1] % 9 != 0 and oct(data[i+1])[-1] == '3') or (data[i+1] % 9 == 0 and data[i] % 9 != 0 and oct(data[i])[-1] == '3'):
         counter += 1
-        if (data[i] < minimum):
-            minimum = data[i]
-        if (data[i + 1] < minimum):
-            minimum = data[i + 1]
+        maximum = max(max(data[i], data[i+1]), maximum)
 
-print(counter, minimum)
+print(counter, maximum)
