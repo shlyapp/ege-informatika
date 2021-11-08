9c68e70520451c7b52e677091073d4de7638b54d
shlyapp <shlyapp.exe@gmail.com>
2021-11-08 22:48:47 +0400

    2507 task

--- kpolyakov/Task 24/2507.py
+++ kpolyakov/Task 24/2507.py
@@ -11,4 +11,10 @@ for i in range(1, len(str)):
             max_char = str[i-1]
         counter = 0
 
-print(max_char, maximum, sep='')
+for i in range(1, len(str)):
+    if (str[i] == str[i-1]):
+        counter += 1
+    else:
+        if (counter + 1 == maximum):
+            print(str[i-1], maximum)
+        counter = 0
