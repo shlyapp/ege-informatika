aa2bbc93e91edf05201693bbe640fb8165d7b6da
shlyapp <shlyapp.exe@gmail.com>
2021-11-09 10:06:45 +0400

    3443 variant 2

--- kpolyakov/Task 23/3443.py
+++ kpolyakov/Task 23/3443.py
@@ -1,17 +1,16 @@
-s=[]
+from functools import lru_cache
+@lru_cache
 def f(x, y,z): 
+    
     if x>y:
         return 0
     elif x==y:
-        s.append(z)
+        print(z)
         return 1
     
-    elif z<10: 
+    elif x<y: 
         f(x * 3, y,z+ 1)  
         f(x + 5, y,z+ 1) 
         f(x + 1, y, z+1) 
       
-    
-
 f(1, 227,0)
-print(min(s))   
