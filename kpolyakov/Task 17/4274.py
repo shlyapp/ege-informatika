9704265cf116ee2963f43979e433619c46635447
shlyapp <shlyapp.exe@gmail.com>
2021-11-28 19:48:00 +0400

    wrong 4274 task

--- kpolyakov/Task 17/4274.py
+++ kpolyakov/Task 17/4274.py
@@ -4,14 +4,9 @@ data = list(map(int, file.readlines()[:-1]))
 counter = 0
 maximum = min(data)
 
-for i in range(1, len(data) - 2):
+for i in range(0, len(data) - 1):
     if (data[i - 1] > data[i] and data[i + 1] > data[i]):
         counter += 1
         maximum = max(maximum, data[i])
-
-if (data[0] < data[1]):
-    counter += 1
-if (data[-2] < data[-1]):
-    counter += 1
 
 print(counter, maximum)
