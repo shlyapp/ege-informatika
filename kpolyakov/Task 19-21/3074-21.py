67d9bf932bd4644710c56f809b1eb9d45876ce03
shlyapp <shlyapp.exe@gmail.com>
2021-11-29 22:04:22 +0400

    fix 3074-21

--- kpolyakov/Task 19-21/3074-21.py
+++ kpolyakov/Task 19-21/3074-21.py
@@ -14,8 +14,8 @@ def game(heap):
         return 'B1'
     if any(game(move) == 'B1' for move in make_moves(heap)):
         return 'P2'
-    if all(game(move) == 'P1' or game(move) == 'P2' for move in make_moves(heap)):
-        return 'B2'
+    if any(game(move)=='P1' for move in make_moves(heap)) and all(game(move) == 'P1' or game(move) == 'P2' for move in make_moves(heap)):
+        return "B2"
 
 for stone in range(1, 1000):
     if (game(stone) is not None) and (game(stone) == 'B2'):
