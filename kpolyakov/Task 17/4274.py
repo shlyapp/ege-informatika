7478452c6fbe50c369e57a91f93e67b2b27cfe57
shlyapp <shlyapp.exe@gmail.com>
2021-11-30 10:03:27 +0400

    fix 4274

--- kpolyakov/Task 17/4274.py
+++ kpolyakov/Task 17/4274.py
@@ -1,10 +1,13 @@
 file = open('kpolyakov/Task 17/17-1.txt')
-data = list(map(int, file.readlines()[:-1]))
+
+data = []
+for element in file:
+    data.append(int(element))
 
 counter = 0
 maximum = min(data)
 
-for i in range(0, len(data) - 1):
+for i in range(1, len(data) - 1):
     if (data[i - 1] > data[i] and data[i + 1] > data[i]):
         counter += 1
         maximum = max(maximum, data[i])
