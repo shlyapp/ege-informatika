c134fb5b34815f834f7d06bc7d9ce9f13c1d9786
shlyapp <shlyapp.exe@gmail.com>
2021-12-12 17:19:17 +0400

    task 66

---
+++ kpolyakov/Task 19-21/66-19.py
@@ -0,0 +1,25 @@
+from functools import lru_cache
+
+def make_moves(heaps):
+    a, b = heaps
+    return (a + 1, b), (a, b + 1), (a + b, b), (a, b + a)
+
+@lru_cache(None)
+
+def game(heaps):
+    if sum(heaps) >= 81:
+        return "WIN"
+    if any(game(move) == "WIN" for move in make_moves(heaps)):
+        return "P1"
+    if any(game(move) == "P1" for move in make_moves(heaps)):
+        return "B1"
+    if any(game(move) == "B1" for move in make_moves(heaps)):
+        return "P2"
+    if all(game(move) == "P1" or game(move) == "P2" for move in make_moves(heaps)):
+        return 'B2'
+
+for s in range(1, 74):
+    heaps = (7, s)
+    if (game(heaps) == "B1"):
+        print(s)
+        break
