0fd4207f065871d95b6e9a0c85505bd5a1842951
shlyapp <shlyapp.exe@gmail.com>
2021-12-12 18:15:40 +0400

    task 38

---
+++ kpolyakov/Task 19-21/38-20.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+
+def make_moves(heap):
+    return (i for i in (heap + 1, heap + 3, heap * 3) if i % 2 != 0)
+
+@lru_cache(None)
+
+def game(heap):
+    if heap >= 51:
+        return "WIN"
+    if any(game(move) == "WIN" for move in make_moves(heap)):
+        return "P1"
+    if all(game(move) == "P1" for move in make_moves(heap)):
+        return "B1"
+    if any(game(move) == "B1" for move in make_moves(heap)):
+        return "P2"
+    if all(game(move) == "P1" or game(move) == "P2" for move in make_moves(heap)):
+        return "B2"
+
+for s in range(1, 51):
+    if (game(s) == "P2"):
+        print(s)
