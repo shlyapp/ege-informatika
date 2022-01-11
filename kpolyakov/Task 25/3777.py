3672ad819bbf084d53b4e2f8f5bb60240d33a10c
shlyapp <shlyapp.exe@gmail.com>
2022-01-11 09:36:53 +0400

    update 3777

--- kpolyakov/Task 25/3777.py
+++ kpolyakov/Task 25/3777.py
@@ -1,8 +1,15 @@
-for i in range(1, 1000):
-    divisors = []
-    for x in range(1, i + 1):
-        if i % x == 0:
-            divisors.append(x)
-    if (sum(list(map(lambda x: x % 2, divisors))) == 5):
-        print(i)
-        
+b = []
+for x in range(3, 88):
+    counter = 0
+    for i in range(1, x + 1):
+        if (x % i == 0):
+            counter += 1
+    if (counter == 2):
+        b.append(x)
+
+for i in b:
+    x = i ** 4
+    while x < 55000000:
+        x = x * 2
+    if  x <= 60000000:
+        print(x, i**4)
