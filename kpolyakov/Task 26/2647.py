5a8ebb095b178d1ed7bd68736b838f5c4527b66f
shlyapp <shlyapp.exe@gmail.com>
2022-01-24 23:24:51 +0400

    task 2647

--- kpolyakov/Task 26/2647.py
+++ kpolyakov/Task 26/2647.py
@@ -1,17 +1,16 @@
-#9954
 file = open('kpolyakov/Task 26/26-j5.txt')
-data = [int(x) for x in file]
-data2 = [0 for i in range(len(data))]
+data1 = [int(x) for x in file]
+data2 = [data1[i] for i in range(len(data1))]
 
-for i in range(1, len(data)-1):
-    data2[i] = sorted([data[i-1], data[i], data[i + 1]])[1]
+for i in range(len(data1)-2):
+    data2[i + 1] = sorted(data1[i:i+3])[1]
 
-counter = 0
-for i in range(1, len(data)-1):
-    counter += data2[i]-data[i]
+summa = 0
 
-print(counter)
+minim = min(data2)
 
-# сделать тестовый код для примера
-# из задачи, а потом уже брать весь
-# массив данных
+for i in range(len(data1)):
+   if data1[i] > data2[i]:
+       summa += data1[i] - data2[i]
+
+print(data2[1:-1].count(minim), summa)
