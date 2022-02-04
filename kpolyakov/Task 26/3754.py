2ee0582f622667481c812dbb760d100cb974616d
shlyapp <shlyapp.exe@gmail.com>
2022-02-04 11:39:30 +0400

    task 3754

--- kpolyakov/Task 26/3754.py
+++ kpolyakov/Task 26/3754.py
@@ -1,38 +1,21 @@
-file = open("kpolyakov/Task 26/26-42.txt")
-# 500 4000000
-N = 4000000
-data = []
+with open("kpolyakov/Task 26/26-42.txt") as F:
+  N, S = map(int, F.readline().split())
 
-for element in file:
-    triple = [x for x in element.split()]
-    triple[1] = int(triple[1]); triple[2] = int(triple[2])
-    data.append(triple)
+  data = []
+  for i in range(N):
+    model, price, size = F.readline().split()
+    data.append( (model, int(price), int(size)) )
 
-data = sorted(data)[::-1]
-summa_Z = 0
+data.sort( key = lambda d: (d[0], -d[1]), reverse = True )
 
-for element in data:
-    if element[0] == 'Z':
-        summa_Z += element[1] * element[2]
-    else:
-        index = data.index(element)
-        break
+xSum = 0
+countA = 0
+for d in data:
+  if xSum+d[1] > S: break
+  thisCount = min(d[2], int((S-xSum)/d[1]))
+  xSum += d[1]*thisCount
+  print( d, '+' + str(thisCount) )
+  if d[0] == 'A':
+    countA += thisCount
 
-ost = N - summa_Z
-data = sorted(data[index : N])
-data2 = []
-
-counter = 0
-
-for element in data:
-    # summa = element[1] * element[2]
-    # if ost - summa > 0:
-    #     counter += element[2]
-    data2.append([element[2], element[1]])
-
-data2 = sorted(data2)
-
-for element in data2:
-    print(element)
-
-print(counter)
+print( countA, S-xSum )
