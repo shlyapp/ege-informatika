d4a411fb7ae0d912213e1a5d799540c60307c026
shlyapp <shlyapp.exe@gmail.com>
2022-04-10 15:29:17 +0400

    2723 task

---
+++ kompege/2723-A.py
@@ -0,0 +1,14 @@
+import itertools
+
+with open('kompege/27A-2723.txt') as file:
+    N = int(file.readline())
+    data = [int(num) for num in file.readlines()]
+
+counter = 0
+
+for trinity in itertools.combinations(data, 3):
+    if all(map(lambda x: x % 19 == 0, trinity)):
+        counter += 1
+        print(trinity)
+
+print(counter)
