716be782ba67223171a340614abac6231d96a51f
shlyapp <shlyapp.exe@gmail.com>
2022-04-14 12:57:35 +0400

    2732 task

---
+++ kompege/2732-A.py
@@ -0,0 +1,13 @@
+import itertools
+
+with open('kompege/27A-2732.txt') as file:
+    N = int(file.readline())
+    data = [int(num) for num in file.readlines()]
+
+maximum = 0
+
+for a, b in itertools.combinations(data, 2):
+    if abs(a - b) % 80 == 0:
+        maximum = max(maximum, abs(a - b))
+
+print(maximum)
