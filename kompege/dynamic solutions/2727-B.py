027933260aec7f74d5d8fff1eda29bc9dfbebeb8
shlyapp <shlyapp.exe@gmail.com>
2022-04-21 15:14:30 +0400

    2727 solve

--- kompege/dynamic solutions/2727-B.py
+++ kompege/dynamic solutions/2727-B.py
@@ -1,20 +1,16 @@
 file = open('kompege/files/27B-2727.txt')
 N = int(file.readline())
-# предыдущие минимальные числа
 k = [float('inf'), float('inf')]
 minimum = float('inf')
 
 for i in range(N):
     num = int(file.readline())
-    if num % 2 == 0:
-        minimum = min((k[0] * num, k[1] * num, minimum))
-    else:
-        minimum = min((k[0] * num, minimum))
 
-    if num % 2 == 0:
-        k[0] = min(k[0], num)
-    else:
-        k[1] = min(k[1], num)
+    if num % 31 == 0:
+        minimum = min(k[1] * num, minimum)
+    minimum = min((k[0] * num, minimum))
+
+    index = 0 if num % 31 == 0 else 1
+    k[index] = min(k[index], num)
 
 print(minimum)
-# 21831874
