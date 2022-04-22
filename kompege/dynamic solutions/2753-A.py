5689769bac146d912247f855179c1ec0685edf4d
shlyapp <shlyapp.exe@gmail.com>
2022-04-22 15:24:58 +0400

    2753 solve

--- kompege/dynamic solutions/2753-A.py
+++ kompege/dynamic solutions/2753-A.py
@@ -1,11 +1,11 @@
 file = open('kompege/files/27A-2753.txt')
 N = int(file.readline())
-data = [int(num) for num in file]
+data = [int(num) for num in file.readlines()]
 counter = 0
 
 for i in range(N):
     for j in range(i + 1, N):
-        if (j - i) <= 7 and (data[i] + data[i]) % 8 != 0:
+        if (j - i) <= 7 and (data[i] + data[j]) % 8 != 0:
             counter += 1
 
 print(counter)
--- kompege/dynamic solutions/2753-B.py
+++ kompege/dynamic solutions/2753-B.py
@@ -1,14 +1,12 @@
-# если произведение остаков кратно 8, то сумма делится на 8
 file = open('kompege/files/27B-2753.txt')
 N = int(file.readline())
-data = [int(num) for num in file]
+data = [int(num) for num in file.readlines()]
 counter = 0
-end = [0 for i in range(10)]
 
 for i in range(N):
     for j in range(i + 1, N):
-        if (j - i) > 7: break
-
+        if (j - i) > 7:
+            break
         if (data[i] + data[j]) % 8 != 0:
             counter += 1
 
