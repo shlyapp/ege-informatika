fd8f7e05d261005b7baa223bcbbb94cc011ed7c6
shlyapp <shlyapp.exe@gmail.com>
2022-05-13 12:33:32 +0400

    2672 task

--- kpolyakov/Task 27/2672-B.py
+++ kpolyakov/Task 27/2672-B.py
@@ -1,16 +1,16 @@
-file = open('kpolyakov/Task 27/27A-2672.txt')
+file = open('kpolyakov/Task 27/27B-2672.txt')
 N = int(file.readline())
 k_6 = k_2 = k_3 = 0
 counter = 0
 
 for i in range(N):
     num = int(file.readline())
-    if num % 2 == 0:
+    if num % 6 == 0:
+        counter += i
+    elif num % 2 == 0:
         counter += k_3
     elif num % 3 == 0:
         counter += k_2
-    elif num % 6 == 0:
-        counter += i
     else:
         counter += k_6
 
