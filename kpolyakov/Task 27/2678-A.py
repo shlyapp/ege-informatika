955d6fb856aed710c598b6e5caa988004abf5d35
shlyapp <shlyapp.exe@gmail.com>
2022-05-13 15:58:06 +0400

    2678 task

---
+++ kpolyakov/Task 27/2678-A.py
@@ -0,0 +1,11 @@
+file = open('kpolyakov/Task 27/27A-2678.txt')
+N = int(file.readline())
+data = [int(num) for num in file.readlines()]
+counter = 0
+
+for i in range(N):
+    for j in range(i + 1, N):
+        if j - i < 5 and (data[i] * data[j]) % 13 == 0 and (data[i] + data[j]) % 2 != 0:
+            counter += 1
+            
+print(counter)
